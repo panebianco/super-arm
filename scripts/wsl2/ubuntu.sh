00fc9e9e0d4afa8c1507c71e28d89f7b804ad265
Whitebread <williampanebianco@gmail.com>
2020-06-10 23:27:09 -0400

    Generate ssh key pair for WSL 2

--- scripts/wsl2/ubuntu.sh
+++ scripts/wsl2/ubuntu.sh
@@ -1,22 +1,30 @@
 #!/bin/bash
 
-echo "Configuring git...what is your user.email address..."
+echo "Configuring environment..."
+
+echo "Configuring git...what is your user.email address?"
 read email
 git config --global user.email $email
-echo "Configuring git...what is your user.name..."
+echo "Configuring git...what is your user.name?"
 read name
 git config --global user.name $name
 
+echo "Creating ssh key pair..."
+ssh-keygen -t rsa -b 4096 -f ~/.ssh/id_rsa -q -N ""
+echo "Public ssh key output below."
+cat ~/.ssh/id_rsa.pub
+
+echo "Updating package manages..."
 sudo apt update
 
+echo "Installing additional packages..."
 sudo apt install git -y
 
-# Install latest version of node (as of 2020-10-06)
-curl -sL https://deb.nodesource.com/setup_14.x | sudo -E bash -
+echo "Configure nodejs...what major version of nodejs (e.g. 14)?"
+read nodejs
+curl -sL "https://deb.nodesource.com/setup_${nodejs}.x" | sudo -E bash -
 sudo apt install nodejs -y
 
 sudo apt install npm -y
-
 sudo apt install awscli -y
-
 sudo apt install jq -y
