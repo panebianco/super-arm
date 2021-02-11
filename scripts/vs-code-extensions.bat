56f5f9d9ef21287102117e73cb6c617ec1f39fb7
Whitebread <williampanebianco@gmail.com>
2021-02-11 13:09:09 -0500

    Update extensions
    Remove out-of-date or no longer used extensions and add new extensions

--- scripts/vs-code-extensions.bat
+++ scripts/vs-code-extensions.bat
@@ -14,9 +14,7 @@ CALL code --install-extension ms-vscode-remote.remote-wsl
 CALL code --install-extension msjsdiag.debugger-for-chrome
 CALL code --install-extension ritwickdey.liveserver
 CALL code --install-extension dbaeumer.vscode-eslint
-CALL code --install-extension formulahendry.auto-close-tag
-CALL code --install-extension formulahendry.auto-rename-tag
-CALL code --install-extension abusaidm.html-snippets
+CALL code --install-extension ecmel.vscode-html-css
 CALL code --install-extension xabikos.javascriptsnippets
 CALL code --install-extension aws-amplify.aws-amplify-vscode
 
@@ -31,21 +29,17 @@ CALL code --install-extension 42crunch.vscode-openapi
 CALL code --install-extension redhat.vscode-xml
 CALL code --install-extension redhat.vscode-yaml
 
-:: Install aws extensions
-CALL code --install-extension loganarnett.lambda-snippet
-
 :: Install infrastructure orchestration extensions
 CALL code --install-extension mauve.terraform
 CALL code --install-extension aws-scripting-guy.cform
 
-:: Install configuration management extensions
-CALL code --install-extension vscoss.vscode-ansible
-CALL code --install-extension puppet.puppet-vscode
-
 :: Install miscellaneous extensions
+CALL code --install-extension visualstudioexptteam.vscodeintellicode
 CALL code --install-extension esbenp.prettier-vscode
-CALL code --install-extension coenraads.bracket-pair-colorizer
+CALL code --install-extension coenraads.bracket-pair-colorizer-2
+CALL code --install-extension christian-kohler.path-intellisense
 CALL code --install-extension asciidoctor.asciidoctor-vscode
 CALL code --install-extension alefragnani.bookmarks
+CALL code --install-extension wayou.vscode-todo-highlight
 CALL code --install-extension hangxingliu.vscode-systemd-support
 CALL code --install-extension streetsidesoftware.code-spell-checker
